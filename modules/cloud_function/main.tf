10d8977aed7daeb4ce921b054e739333efdffc1c
n cha <n.cha@example.com>
2023-11-19 23:35:36 +0200

    try to set iam for cloud function to be condiational too

--- modules/cloud_function/main.tf
+++ modules/cloud_function/main.tf
@@ -1,12 +1,10 @@
 resource "google_cloudfunctions_function" "default" {
-  count = var.cf_zip_file != "" ? 1 : 0
-
   name                  = var.cf_name
   description           = "Cloud Function"
   runtime               = var.cf_runtime
   available_memory_mb   = var.cf_ram
   source_archive_bucket = var.cf_bucket_name
-  source_archive_object = var.cf_zip_file
+  source_archive_object = var.cf_zip_file != "" ? var.cf_zip_file : data.google_cloudfunctions_function.default.source_archive_object
   entry_point           = var.cf_entrypoint
   trigger_http          = true
 
@@ -15,12 +13,8 @@ resource "google_cloudfunctions_function" "default" {
   }
 }
 
-resource "google_cloudfunctions_function_iam_member" "public_invoker" {
-  count = var.cf_zip_file != "" ? 1 : 0
-
-  project        = var.gcp_project
-  region         = var.region
-  cloud_function = google_cloudfunctions_function.default[0].name
-  role           = "roles/cloudfunctions.invoker"
-  member         = "allUsers"
+data "google_cloudfunctions_function" "default" {
+  name    = var.cf_name
+  project = var.gcp_project
+  region  = var.region
 }
